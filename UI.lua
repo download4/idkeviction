1e786e83c354518fb5fa8aab697d30a00aeaa59c
download4 <47045675+download4@users.noreply.github.com>
2021-12-28 14:32:43 -0600

    Delete UI.lua

--- UI.lua
+++
@@ -1,8 +0,0 @@
-local GithubURL = "https://raw.githubusercontent.com/download4/idkeviction"
-local Branch = "beta" -- switch to main when not in beta
-
-local UILib = loadstring(game:HttpGet(("%s/%s/UILib.lua"):format(GitHubUrl, Branch)))()
-local Configuration = loadstring(game:HttpGet(("%s/%s/Configuration.lua"):format(GitHubUrl, Branch)))()
-
-local MainWindow = UILib:AddWindow(Configuration.MainWindow.Title)
-local Challenges = MainWindow:AddTab(Configuration.Challenges.Title)
