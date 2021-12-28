c2bb969165092d0eb0201b7f4b54dbba1438eeb0
download4 <47045675+download4@users.noreply.github.com>
2021-12-28 17:15:43 -0600

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -7,7 +7,12 @@ end
 
 local UiLibrary = loadstring(getFile("UILib.lua"))()
 
-print(UiLibrary)
-
 local MainWindow = UiLibrary:AddWindow("Eviction Notice")
 local ChallengesFolder = MainWindow:AddTab("Challenges")
+
+local TeleportSpeed = 32
+local SpeedSlider = ChallengesFolder:AddSlider("TeleportSpeed", function(num)
+    TeleportSpeed = num
+end, {min = 16, max = 100, readonly = false})
+
+SpeedSlider:Set(32)
