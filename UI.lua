c157062ecba6ba2f8fea617bd7bf34c9da084554
download4 <47045675+download4@users.noreply.github.com>
2021-12-28 14:20:46 -0600

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -1,1 +1,7 @@
--- not sure yet
+local GithubURL = "https://raw.githubusercontent.com/download4/idkeviction"
+local Branch = "beta" -- switch to main when not in beta
+
+local UILib = loadstring(game:HttpGet(("%s/%s/UILib.lua"):format(GitHubUrl, Branch)))()
+local Configuration = loadstring(game:HttpGet(("%s/%s/Configuration.lua"):format(GitHubUrl, Branch)))()
+
+UILib:AddWindow("Games", Configuration.GamesWindow)
