d563dac30a0c3bc78e09d9252cc92042d4724060
download4 <47045675+download4@users.noreply.github.com>
2021-12-28 17:35:21 -0600

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,8 +1,3 @@
-if game.PlaceId ~= 8388707335 then
-    game.ReplicatedStorage.Hub.Join:InvokeSeerver()
-    return
-end
-
 local githubBranch = "beta"
 local githubUrl = "https://raw.githubusercontent.com/download4/idkeviction/" .. githubBranch .. "/"
 
@@ -23,4 +18,16 @@ local SpeedSlider = MiscFolder:AddSlider("Teleport Speed", function(num)
     TeleportSpeed = num
 end, {min = 32, max = 100})
 
+SpeedSlider:Set(32)
+
 ChallengesFolder:AddLabel("Challenge List")
+ChallengesFolder:AddButton("Stratosfear", function()
+    workspace.House.Stratosfear.Kill.TouchInterest:Destroy() -- remove the touch interest
+end)
+
+MiscFolder:AddButton("Crash Challenge", function()
+    local ChosenEvent = game.ReplicatedStorage.Comps:GetChildren()[math.random(1,#game.ReplicatedStorage.Comps:GetChildren())]
+    for i = 1, 200 do
+        ChosenEvent:FireServer()
+    end
+end)
