bc51d1ea3f7f936ceb72ee1d9cf6a0c12e7a561b
download4 <47045675+download4@users.noreply.github.com>
2021-12-28 14:21:48 -0600

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -4,4 +4,4 @@ local Branch = "beta" -- switch to main when not in beta
 local UILib = loadstring(game:HttpGet(("%s/%s/UILib.lua"):format(GitHubUrl, Branch)))()
 local Configuration = loadstring(game:HttpGet(("%s/%s/Configuration.lua"):format(GitHubUrl, Branch)))()
 
-UILib:AddWindow("Games", Configuration.GamesWindow)
+UILib:AddWindow(Configuration.GamesWindow.Title, Configuration.GamesWindow)
