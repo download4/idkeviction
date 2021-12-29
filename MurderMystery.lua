5b4dbc1803178e0612bd56e8e13bd746cd9a6fea
download4 <47045675+download4@users.noreply.github.com>
2021-12-28 19:10:11 -0600

    Update MurderMystery.lua

--- MurderMystery.lua
+++ MurderMystery.lua
@@ -10,3 +10,58 @@ local UiLibrary = loadstring(getFile("UILib.lua"))()
 local MainWindow = UiLibrary:AddWindow("Murder Mystery")
 local MainFolder = MainWindow:AddTab("Main")
 local MiscFolder = MainWindow:AddTab("Misc")
+
+local MurderLabel = MainFolder:AddLabel("Murderer: idk")
+local SherrifLabel = MainFolder:AddLabel("Sherrif: idk")
+
+function findM()
+
+    local m = nil
+
+    repeat
+        for _,v in ipairs(game.Players:GetPlayers()) do
+            if v.Backpack:FindFirstChild("Knife") then
+    
+                m = v
+                break
+    
+            end
+        end
+        task.wait()
+    until m ~= nil
+
+    return m
+    -- bad
+
+end
+
+function findS()
+    
+    local m = nil
+
+    repeat
+        for _,v in ipairs(game.Players:GetPlayers()) do
+            if v.Backpack:FindFirstChild("Gun") then
+    
+                m = v
+                break
+    
+            end
+        end
+        task.wait()
+    until m ~= nil
+
+    return m
+
+    -- bad
+
+end
+
+game.ReplicatedStorage.RoleSelect.OnClientEvent:Connect(function()
+    
+    -- game started
+    MurderLabel.Text = ("Murderer: " .. findM())
+    SherrifLabel.Text = ("Sherrif: " .. findS())
+
+
+end)
