6a4fc8550b29e66ad1ab54d499459c9e9d1659ab
download4 <47045675+download4@users.noreply.github.com>
2021-12-28 19:28:04 -0600

    Update MurderMystery.lua

--- MurderMystery.lua
+++ MurderMystery.lua
@@ -14,6 +14,25 @@ local MiscFolder = MainWindow:AddTab("Misc")
 local MurderLabel = MainFolder:AddLabel("Murderer: idk")
 local SherrifLabel = MainFolder:AddLabel("Sherrif: idk")
 
+local nameDisplayDistance = 0 
+
+function handlePlayer(plr)
+
+    if plr.Character then
+
+        plr.Character:WaitForChild("Humanoid").NameDisplayDistance = nameDisplayDistance
+
+    end
+
+    plr.CharacterAdded:Connect(function(d)
+        d:WaitForChild("Humanoid").NameDisplayDistance = nameDisplayDistance
+    end)
+
+end
+
+for _, plr in ipairs(game.Players:GetPlayers()) do handlePlayer(plr) end
+game.Players.PlayerAdded:Connect(handlePlayer)
+
 function findM()
 
     local m = nil
@@ -60,6 +79,15 @@ end
 MurderLabel.Text = ("Murderer: " .. findM().Name)
 SherrifLabel.Text = ("Sherrif: " .. findS().Name)
 
+MainFolder:AddSwitch("Display Names", function(bool)
+
+    nameDisplayDistance = bool and 100 or 0
+    for _,v in ipairs(game.Players:GetPlayers()) do
+        local char = v.Character or v.CharacterAdded:Wait()
+        char:WaitForChild("Humaoid").NameDisplayDistance = nameDisplayDistance
+    end
+
+end)
 
 game.ReplicatedStorage.RoleSelect.OnClientEvent:Connect(function()
     
